123fbf5ff877625b9bb6b44effdfb089d1d872f7
Perez Sen <103560740+PerezSen@users.noreply.github.com>
2026-02-15 18:58:40 -0600

    Refactor Android version imports for clarity

--- source/import.hx
+++ source/import.hx
@@ -34,11 +34,13 @@ import android.Settings as AndroidSettings;
 import android.widget.Toast as AndroidToast;
 import android.content.Context as AndroidContext;
 import android.Permissions as AndroidPermissions;
-import android.os.Build.VERSION as AndroidVersion;
 import android.os.Environment as AndroidEnvironment;
 import android.os.BatteryManager as AndroidBatteryManager;
-import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.Build;
+typedef AndroidVersion = android.os.Build.VERSION;
+typedef AndroidVersionCode = android.os.Build.VERSION_CODES;
 #end
+
 
 // Lua
 #if LUA_ALLOWED
